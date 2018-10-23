a33a9366e2e223dd9cb2ad0d244ac1be7936097d
Wisely <13920671706@139.com>
2018-10-23 23:20:05 +0800

    无

--- test/wisely/hanshu.py
+++ test/wisely/hanshu.py
@@ -4,3 +4,19 @@ print(abs(1))
 print(abs(-10))
 #print(abs(1,2))
 #print(abs('2'))
+print(max(1,3,5,10))
+print(bool("1"))
+print(bool(1))
+print(bool(''))
+print(bool(""))
+
+a=abs
+print(a(-1))
+m=max
+print(m(2,3,6))
+
+n1 = 255
+n2 = 1000
+
+print(hex(255))
+print(hex(1000))
